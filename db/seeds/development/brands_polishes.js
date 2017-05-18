c87e9fdd8c8ebaa2f9f5ab9f5f7967a6b890c48e
Robbie Jaeger <jaeger.rob@gmail.com>
2017-05-18 14:21:21 -0600

    Change nested thens to return and remove nesting

--- db/seeds/development/brands_polishes.js
+++ db/seeds/development/brands_polishes.js
@@ -2,25 +2,22 @@ const brandsData = require('../../../data/brands');
 const polishesData = require('../../../data/polishes');
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('polishes').del()
     .then(() => {
       return knex('brands').del()
-      .then(() => {
-        // Insert brands
-        return knex('brands').insert(brandsData)
-        .then(() => {
-          // Insert polishes with brand foreign key
-          var polishPromises = [];
-          polishesData.forEach((polish) => {
-            let brand = polish.brand;
-            polishPromises.push(createPolish(knex, polish, brand))
-          });
-
-          return Promise.all(polishPromises);
-        });
+    })
+    .then(() => {
+      return knex('brands').insert(brandsData)
+    })
+    .then(() => {
+      var polishPromises = [];
+      polishesData.forEach((polish) => {
+        let brand = polish.brand;
+        polishPromises.push(createPolish(knex, polish, brand))
       });
-    });
+
+      return Promise.all(polishPromises);
+    })
 };
 
 const createPolish = (knex, polish, brand) => {
